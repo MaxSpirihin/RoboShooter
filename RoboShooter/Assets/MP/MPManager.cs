03bdefe54b34dbfc81ef4e6e7ff9af897d1f9b4b
MaxSpirihin <max.spirihin@gmail.com>
2017-09-04 13:06:37 +0300

    Заготовка мультиплеера Имеем возможность двум игрокам синхронизированно двигаться
    -Импортирован Photon Unity Networking
    -Создана папка MP - здесь все, что для него нужно
    -Создано два скрипта - PlayerMP.cs и MPManager.cs, две сцены и префаб MP игрока
    
    Впереди масса работы
    -Синхронизация анимации и всего отображения
    -Синхронизация выстрелов
    -Подсчет очков

---
+++ RoboShooter/Assets/MP/MPManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MPManager : Photon.PunBehaviour {
+
+    public static MPManager instance;
+    GameObject localPlayer;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+
+        PhotonNetwork.automaticallySyncScene = true;
+
+    }
+
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("RoboShooter_Alpha");
+    }
+
+
+    public void JoinGame()
+    {
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom("Default Room", ro, null);
+    }
+
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            PhotonNetwork.LoadLevel("MP/MPArena");
+        }
+    }
+
+    void OnLevelWasLoaded(int levelNumber)
+    {
+        if (!PhotonNetwork.inRoom) return;
+        Debug.Log(PhotonNetwork.isMasterClient);
+
+        localPlayer = PhotonNetwork.Instantiate(
+            "PlayerMP",
+            new Vector3(0, 100f, 0),
+            Quaternion.identity, 0);
+    }
+}
---
+++ RoboShooter/Assets/MP/PlayerMP.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMP : Photon.PunBehaviour
+{
+    Camera playerCam;
+
+    void Awake()
+    {
+       // DontDestroyOnLoad(gameObject);
+        playerCam = GetComponentInChildren<Camera>();
+
+        if (!photonView.isMine)
+        {
+            playerCam.gameObject.SetActive(false);
+            
+            foreach (var o in GetComponentsInChildren<FpsLook>())
+                o.enabled = false;
+
+            GetComponent<FPSMove>().enabled = false;
+
+        }
+    }
+}
